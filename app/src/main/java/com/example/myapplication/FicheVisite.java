ede51d5264a9550ef1dde23995ed2d94b2d619fc
alory@jolsio.net <atLG100!>
2021-03-16 17:52:21 +0100

    Version 1.1

---
+++ app/src/main/java/com/example/myapplication/FicheVisite.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FicheVisite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiche_visite);
+    }
+}
