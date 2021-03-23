2a3447926ee5fb7fec55ddcc3fa95c6569b8f31a
alory@jolsio.net <atLG100!>
2021-03-23 17:41:32 +0100

    Version 1.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView etudiantsView;
+    private DatabaseManager databaseManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etudiantsView = (TextView) findViewById(R.id.etudiantsView);
+        databaseManager = new DatabaseManager( this);
+
+        databaseManager.insertEtudiants("Landry");
+        databaseManager.insertEtudiants("Landry");
+        databaseManager.insertEtudiants("Landry");
+        databaseManager.insertEtudiants("Landry");
+        databaseManager.insertEtudiants("Landry");
+
+        databaseManager.close();
     }
 }
